28d65872853a8bd456d5e59dc3d349bebb6c8c00
DaniyalArain0903 <152718646+DaniyalArain0903@users.noreply.github.com>
2025-01-09 14:10:00 +0500

    Add files via upload

---
+++ Project 1 ( calculator)/calculator.py
@@ -0,0 +1,107 @@
+
+from tkinter import *
+
+# Create the main window
+root = Tk()
+root.title("Calculator")
+root.geometry("500x450")  # Increased window size
+root.configure(background="#6D7B8D")  
+
+# Global variable to store the current expression
+expression = ""
+ 
+# Function to update the expression variable
+def update_expression(num):
+    global expression
+    expression += str(num)
+    equation.set(expression)
+
+# Function to evaluate the expression
+def evaluate():
+    global expression
+    try:
+        result = str(eval(expression))
+        equation.set(result)
+        expression = result
+    except Exception as e:
+        equation.set("Error")
+        expression = ""
+
+# Function to clear the expression
+def clear():
+    global expression
+    expression = ""
+    equation.set("")
+
+# Function to delete the last character
+def backspace():
+    global expression
+    expression = expression[:-1]
+    equation.set(expression)
+
+# Create a entry field to display the expression
+equation = StringVar()
+expression_field = Entry(root, textvariable=equation, width=35,  font=('Lucida', 18, 'bold'), justify=LEFT, bd=5, cursor="xterm")
+expression_field.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+# Create number buttons
+button_7 = Button(root, text="7", width=8, height=2,bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(7))
+button_7.grid(row=1, column=0, padx=5, pady=5)
+
+button_8 = Button(root, text="8", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(8))
+button_8.grid(row=1, column=1, padx=5, pady=5)
+
+button_9 = Button(root, text="9", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(9))
+button_9.grid(row=1, column=2, padx=5, pady=5)
+
+button_4 = Button(root, text="4", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(4))
+button_4.grid(row=2, column=0, padx=5, pady=5)
+
+button_5 = Button(root, text="5", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(5))
+button_5.grid(row=2, column=1, padx=5, pady=5)
+
+button_6 = Button(root, text="6", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(6))
+button_6.grid(row=2, column=2, padx=5, pady=5)
+
+button_1 = Button(root, text="1", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(1))
+button_1.grid(row=3, column=0, padx=5, pady=5)
+
+button_2 = Button(root, text="2", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(2))
+button_2.grid(row=3, column=1, padx=5, pady=5)
+
+button_3 = Button(root, text="3", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(3))
+button_3.grid(row=3, column=2, padx=5, pady=5)
+
+button_0 = Button(root, text="0", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression(0))
+button_0.grid(row=4, column=0, padx=5, pady=5)
+
+button_dot = Button(root, text=".", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=lambda: update_expression("."))
+button_dot.grid(row=4, column=1, padx=5, pady=5)
+
+# Create operator buttons
+button_add = Button(root, text="+", width=8, height=2, bd=4 , bg="grey", font=('Lucida', 14, 'bold'), command=lambda: update_expression("+"))
+button_add.grid(row=1, column=3, padx=5, pady=5)
+
+button_subtract = Button(root, text="−", width=8, height=2, bd=4 , bg="grey", font=('Lucida', 14, 'bold'), command=lambda: update_expression("-"))
+button_subtract.grid(row=2, column=3, padx=5, pady=5)
+
+button_multiply = Button(root, text="*", width=8, height=2, bd=4 , bg="grey", font=('Lucida', 14, 'bold'), command=lambda: update_expression("*"))
+button_multiply.grid(row=3, column=3, padx=5, pady=5)
+
+button_divide = Button(root, text="/", width=8, height=2, bd=4 , bg="grey", font=('Lucida', 14, 'bold'), command=lambda: update_expression("/"))
+button_divide.grid(row=4, column=3, padx=5, pady=5)
+
+
+button_clear = Button(root, text="C", width=17, height=2, bd=4 , bg="red", font=('Lucida', 14, 'bold'), command=clear)
+button_clear.grid(row=5, column=2, columnspan=2, padx=5, pady=5)
+
+button_equal = Button(root, text="=", width=17, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=evaluate)
+button_equal.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
+
+
+button_backspace = Button(root, text="←", width=8, height=2, bd=4 , font=('Lucida', 14, 'bold'), command=backspace)
+button_backspace.grid(row=4, column=2, padx=5, pady=5)
+
+# Start the main loop
+root.mainloop()
+ 
---
+++ Project 2 ( To Do list )/project2.py
@@ -0,0 +1,148 @@
+import os
+
+class TodoTask:
+    
+    def __init__(self, name, description):
+        
+        self.name = name
+        self.description = description
+        self.completed = False
+        self.next = None
+
+class ToDoList:
+    
+    def __init__(self):
+        
+        self.head = None
+        self.task_id_counter = 1
+
+    def addTask(self, task_name, task_description):
+        
+            
+        new_task = TodoTask(task_name, task_description)
+        new_task.id = self.task_id_counter
+        self.task_id_counter += 1         #Increment Counter
+
+        if self.head is None:
+            self.head = new_task
+        else:
+            curNode = self.head
+            while curNode.next is not None:
+                curNode = curNode.next
+            curNode.next = new_task
+        print("Task added successfully to the to-do list!")
+
+    def deleteTask(self, task_id):
+                
+        if self.head is None:
+            print("No tasks to delete!")
+            return
+
+        if self.head.id == task_id:
+            self.head = self.head.next
+            print("Task deleted successfully!")
+            return
+
+        curNode = self.head
+        prev = None
+        while curNode:
+            if curNode.id == task_id:
+                prev.next = curNode.next
+                print("Task deleted successfully!")
+                return
+            prev = curNode
+            curNode = curNode.next
+        print("Task not found!")
+
+    def searchTask(self, task_id):
+                
+        curNode = self.head
+        while curNode:
+            if curNode.id == task_id:
+                return curNode
+            curNode = curNode.next
+        return None
+
+    def displayTasks(self, show_completed=False):
+        
+        
+        if self.head is None:
+            print("No tasks to display")
+            return
+
+        curNode = self.head
+        while curNode:
+            if show_completed or not curNode.completed:
+                status = "Completed" if curNode.completed else "Not Completed"
+                print(f"Task ID: {curNode.id} - Task: {curNode.name} - Status: {status}")
+            curNode = curNode.next
+
+    def markCompleted(self, task_id):
+        
+        task = self.searchTask(task_id)
+        if task:
+            task.completed = True
+            print("Task marked as completed!")
+        else:
+            print("Task not found!")
+
+            
+            
+def print_menu():
+    print("\nTo-Do List Menu")
+    print("1. Add Your Task")
+    print("2. Display Your Tasks")
+    print("3. Search Your Task")
+    print("4. Delete  Your Task")
+    print("5. Mark Task as Completed")
+    print("6. Quit")
+    print()
+
+if __name__ == "__main__":
+    todo_list = ToDoList()
+
+    while True:
+        print_menu()
+        choice = input("Enter your choice (1-6): ")
+
+        if choice == "1":
+            task_name = input("Enter the task name: ")
+            task_description = input("Enter the task description: ")
+            todo_list.addTask(task_name, task_description)
+
+        elif choice == "2":
+            todo_list.displayTasks()
+
+        elif choice == "3":
+            try:
+                task_id = int(input("Enter the task ID to search: "))
+                task = todo_list.searchTask(task_id)
+                if task:
+                    status = "Completed" if task.completed else "Not Completed"
+                    print(f"Task found: Task ID: {task.id} - Task: {task.name} - Status: {status}")
+                else:
+                    print("Task not found!")
+            except ValueError:
+                print("Invalid input. Please enter a valid task ID.")
+
+        elif choice == "4":
+            try:
+                task_id = int(input("Enter the task ID to delete: "))
+                todo_list.deleteTask(task_id)
+            except ValueError:
+                print("Invalid input. Please enter a valid task ID.")
+
+        elif choice == "5":
+            try:
+                task_id = int(input("Enter the task ID to mark as completed: "))
+                todo_list.markCompleted(task_id)
+            except ValueError:
+                print("Invalid input. Please enter a valid task ID.")
+
+    
+        elif choice == "6":
+            print("Quitting the program...")
+            break
+
+        else:
+            print("Invalid Input! Please try again.")
